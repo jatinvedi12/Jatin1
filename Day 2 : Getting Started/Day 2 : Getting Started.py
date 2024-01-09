84a89687220197c329a505e7f34ddef8277ed442
jatinvedi12 <jatinvedi12>
2024-01-09 09:28:26 +0000

    Commit Day 2 files

---
+++ Day 2 : Getting Started/Day 2 : Getting Started.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Getting started with spark
+# MAGIC ## Databricks
+# MAGIC
+
+# COMMAND ----------
+
+print("Run Python")
+
+# COMMAND ----------
+
+
